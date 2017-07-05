05666c68de7f1d1c74929bb71aa2f84c862d26ae
vikrant arankalle <vikrantarankalle@gmail.com>
2017-07-05 15:04:35 +0530

    adding text via .java file

--- app/src/main/java/com/mybus/HomeActivity.java
+++ app/src/main/java/com/mybus/HomeActivity.java
@@ -1,13 +1,18 @@
 package com.mybus;
 
+import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        textView=(TextView)findViewById(R.id.tv);
+        textView.setText("Hello Welcome To hell");
     }
 }
