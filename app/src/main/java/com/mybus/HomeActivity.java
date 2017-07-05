18eb0e6bfbe6a1058c92036a131edecce966f2b3
vikrant arankalle <vikrantarankalle@gmail.com>
2017-07-05 15:15:22 +0530

    button added and event also

--- app/src/main/java/com/mybus/HomeActivity.java
+++ app/src/main/java/com/mybus/HomeActivity.java
@@ -3,16 +3,26 @@ package com.mybus;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
     private TextView textView;
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         textView=(TextView)findViewById(R.id.tv);
+        button=(Button)findViewById(R.id.button);
         textView.setText("Hello Welcome To hell");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Not that hard ....");
+            }
+        });
     }
 }
